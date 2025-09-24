4293cf696586757fa5ba3a38073dc8f6f786e9af
Felix Broberg <165168078+Feel1x@users.noreply.github.com>
2025-09-24 12:53:47 +0200

    added test for if full room equal true

--- zombiesCS.Tests/ZombiesTests.cs
+++ zombiesCS.Tests/ZombiesTests.cs
@@ -1,40 +1,46 @@
-namespace zombiesCS.Tests
+using zombiesCS;
+
+public class ZombiesTests
 {
-    public class ZombiesTests
+    [Fact]
+    public void RoomIsFull()
     {
-        [Fact]
-        public void RoomIsFull()
-        {
-        }
+        //arrange
+        var room = new Room(0);
 
-        [Fact]
-        public void EmptyRoomThatFitsOneZombie_IsNotFull()
-        {
-            // Given
+        //act
+        var isRoomFull = room.isFull();
 
-            // When
+        //assert
+        Assert.True(isRoomFull);
+    }
 
-            // Then
-        }
+    [Fact]
+    public void EmptyRoomThatFitsOneZombie_IsNotFull()
+    {
 
-        [Fact]
-        public void RoomWithNoCapacity_CannotFitAnyZombies()
-        {
-        }
+    }
 
-        [Fact]
-        public void OneRoomerBecomesFull_WhenAZombieIsAdded()
-        {
-        }
+    [Fact]
+    public void RoomWithNoCapacity_CannotFitAnyZombies()
+    {
+    }
 
-        [Fact]
-        public void TwoRoomerIsNotFull_WhenAZombieIsAdded()
-        {
-        }
-    
-        [Fact]
-        public void SecondZombieConsumesFirstZombie_WhenAddedToAOneRoomer()
-        {
-        }
+    [Fact]
+    public void OneRoomerBecomesFull_WhenAZombieIsAdded()
+    {
+    }
+
+    [Fact]
+    public void TwoRoomerIsNotFull_WhenAZombieIsAdded()
+    {
+    }
+
+    [Fact]
+    public void SecondZombieConsumesFirstZombie_WhenAddedToAOneRoomer()
+    {
     }
 }
+
+
+
